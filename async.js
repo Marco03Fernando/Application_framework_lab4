a1fcff81de01e4409f23c4f17227dbcb36804e64
Marco Fernando <marcofernando.dev@gmail.com>
2026-02-22 11:44:07 +0530

    fixed async

--- async.js
+++ async.js
@@ -1,9 +1,17 @@
+const condition = true; // change to false to test the catch()
+
+const myPromise = new Promise((resolve, reject) => {
+  if (condition) resolve("Success!");
+  else reject("Failure!");
+});
+
 async function myFunction() {
- try {
- const result = await myPromise;
- console.log(result);
- } catch (error) {
- console.log(error);
- }
+  try {
+    const result = await myPromise;
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
 }
+
 myFunction();

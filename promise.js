ba7d04460f649d23a321a03522086021b46b4357
Marco Fernando <marcofernando.dev@gmail.com>
2026-02-22 11:37:08 +0530

    Fix promise condition example

--- promise.js
+++ promise.js
@@ -1,12 +1,13 @@
+const condition = true; // change to false to test reject
+
 const myPromise = new Promise((resolve, reject) => {
- if (condition) {
- resolve('Success!');
- } else {
- reject('Failure!');
- }
+  if (condition) {
+    resolve('Success!');
+  } else {
+    reject('Failure!');
+  }
 });
-myPromise.then((result) => {
- console.log(result);
-}).catch((error) => {
- console.log(error);
-});
+
+myPromise
+  .then((result) => console.log(result))
+  .catch((error) => console.log(error));
